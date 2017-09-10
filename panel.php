770b6838797c50189eb1be6a9cb520420aace62b
sarziv <saruzivila12@gmail.com>
2017-09-11 01:22:07 +0300

    market101
    framework made type/stuff.. and stuff

---
+++ panel.php
@@ -0,0 +1,35 @@
+<html>
+<?php include 'head.html';?>
+<body>
+		
+<div id="wrapper">
+
+<!-- Sidebar -->
+		<?php include 'sidePanel/menu.html';?>
+<!-- /#sidebar-wrapper -->
+
+<div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+		<?php include 'button/buttonMenuCp.html';?>
+
+			<div class="padd" >
+		<?php include 'content/cont_panel_1.html';?>
+			</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /#wrapper -->
+	<script src="js/jquery.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
+</body>
+</html>
